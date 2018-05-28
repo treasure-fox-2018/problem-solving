fc4c3726c9f9db0587efbf16fecef03c6d9adc9d
wisnugautama <ngurahwisnugautama@gmail.com>
2018-05-28 14:10:44 +0700

    Revisi exercise problem-solving done

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
-SET IndexTono with 0
-SET IndexAnton with 0
-SET IndexBudi with 0
-SET i with 7
+//PSEUDOCODE
+// SET IndexTono with 0
+// SET IndexAnton with 0
+// SET IndexBudi with 0
+// SET i with 7
+//
+// FOR i to 31 DO
+//   SET 'output' with string
+//
+//   IF i equal 7 DO
+//     DISPLAY 'tanggal' ADD i ADD ': ' ADD 'Tono,Anton,Budi'
+//   ELSE
+//     IndexTono ADD 1
+//     IndexAnton ADD 1
+//     IndexBudi ADD 1
+//
+//     IF i MOD 5 equal 0 DO
+//       output ADD 'Tempat fitnes libur'
+//     END IF
+//
+//     IF IndexTono equal 2 DO
+//       output ADD 'Tono,'
+//       RESET IndexTono
+//     END IF
+//
+//     IF IndexAnton equal 4 DO
+//       output ADD 'Anton,'
+//       RESET IndexAnton
+//     END IF
+//
+//     IF IndexBudi equal 5 DO
+//       output ADD 'Budi,'
+//       RESET IndexBudi
+//     END IF
+//     DISPLAY 'tanggal' ADD i ADD ': ' ADD output
+//   END IF
+// END FOR
+var indexTono = 0;
+var indexAnton = 0;
+var indexBudi = 0;
 
-FOR i to 31 DO
-  SET 'output' with string
+for (var i = 7; i <= 31; i++) {
+  var output = '';
 
-  IF i equal 7 DO
-    DISPLAY 'tanggal' ADD i ADD ': ' ADD 'Tono,Anton,Budi'
-  ELSE
-    IndexTono ADD 1
-    IndexAnton ADD 1
-    IndexBudi ADD 1
+  if (i === 7) {
+    console.log('tanggal ' + i + ': '+ 'Tono,Anton,Budi');
+  }else{
+    indexTono += 1;
+    indexAnton += 1;
+    indexBudi += 1;
 
-    IF i MOD 5 equal 0 DO
-      output ADD 'Tempat fitnes libur'
-    END IF
+    if (i % 5 === 0) {
+      output += 'Tempat fitnes libur';
+    }
 
-    IF IndexTono equal 2 DO
-      output ADD 'Tono,'
-      RESET IndexTono
-    END IF
+    if (indexTono === 2) {
+      output += 'Tono,';
+      indexTono = 0;
+    }
 
-    IF IndexAnton equal 4 DO
-      output ADD 'Anton,'
-      RESET IndexAnton
-    END IF
+    if (indexAnton === 4) {
+      output += 'Anton,'
+      indexAnton = 0;
+    }
 
-    IF IndexBudi equal 5 DO
-      output ADD 'Budi,'
-      RESET IndexBudi
-    END IF
-    DISPLAY 'tanggal' ADD i ADD ': ' ADD output
-  END IF
-END FOR
+    if (indexBudi === 5) {
+      output += 'Budi,'
+      indexBudi = 0;
+    }
+    console.log('tanggal ' + i + ': '+ output);
+  }
+}
