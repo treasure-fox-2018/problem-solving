ed532ea8dd78e74585e64e88b38ac84fffbe5ae3
brian-p <hacktiv8@H8JKT002.localdomain>
2018-05-28 14:33:45 +0700

    Challenge done with pseudocode

--- problemSolving.js
+++ problemSolving.js
@@ -1,3 +1,18 @@
+/* PSEUDOCODE
+1. SET count with 0
+2. FOR a = 7 to 31
+    2a. SET member with ''
+    2b. IF a modulus 5 not equal with 0 THEN
+        2b.a. IF count modulus 2 equal with 0 THEN
+            ADD member with 'Tono'
+        2b.b. IF count modulus 4 equal with 0 THEN
+            ADD member with 'Anton'
+        2b.c. IF count modulus 5 equal with 0 THEN
+            ADD member with 'Budi'
+        2b.d. DISPLAY 'Tanggal' with value of a with ':' with member indeks 0 untill member indeks length of member -1
+    2c. ELSE DISPLAY 'Tanggal' with value of a with ':' with 'Tempat Fitness Tutup'
+    2d. INCREMENT count
+*/
 var count = 0
 for(var a = 7; a <= 31; a++){
   var member = ''
